5b997ff85c287b83481ab07562ea3952114cade2
Archangel <nathapcal@gmail.com>
2020-09-02 17:16:22 -0400

    removed pseudocode (unnessary and unprofessional), added variables to store prompt and confirmed elements

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-//Psuedocode
-
-// Add onclickEvent to HTML to initalize the password generation 
-
-// Use DOM to aim for the button and add a on-click event to trigger questions
-
-//Prompt and confirm questions to user preference
-//First question to prompt: How many characters would the user like the password to be?
-//Second question to confirm:  would you like to include lowercase?
-//Third question to confirm: would you like to include uppercase?
-//Fourth question to confirm: would you like to include numeric values?
-//Fifth questions to confirm: would you like to include special characters?
-
-
-// With each prompt, set as an array.  
-// With each confirm, set these out to variables of booleans 
-// create for loops to generate random information for using the input from the arrays and booleans and push them into their arrays
-// return combined generated password to the textarea
-
-// function for the onClickEvent
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -28,26 +7,38 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-function generatePassword(onclick="generateBtn") {
+function generatePassword(onclick = "generateBtn") {
+  //
 
-  // prompts question for how long the password would be
+  // Prompts question for how long the password would be and stores value
   var passwordLength = prompt("How long you you like the password to be");
-  // lets the user to choose to use upper case
+  // Sets Boolean
+  var lowerCase = true;
+  // lets the user to coose to use upper case and stores boolean
   var passwordLowerCase = confirm("Would you like to include lower cases?");
-  // lets the user to choose to use lower case
+  // Sets boolean
+  var upperCase = true;
+  // lets the user to choose to use lower case and stores boolean
   var passwordUpperCase = confirm("Would you like to include upper cases?");
-  // lets the user to choose to use numbers
-  var passwordNumbers = confirm("Would you like to include numbers?")
-  // lets the user to choose to use special characters
-  var passwordCharacters = confirm("Would you like to include special characters?");
+  // Sets boolean
+  var numbers = true;
+  // lets the user to choose to use numbers and stores boolean.
+  var passwordNumbers = confirm("Would you like to include numbers?");
+  // Sets boolean
+  var characters = true;
+  // lets the user to choose to use special characters and stores booleans;
+  var passwordCharacters = confirm(
+    "Would you like to include special characters?"
+  );
 }
 
+//generating random numbers
+for (var i = 0; i <= passwordNumbers.length; i++) {
+  var passwordNumbersAnswer = Math.floor(Math.random() * 128 - 1);
+  console.log(passwordNumbersAnswer[i]);
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
