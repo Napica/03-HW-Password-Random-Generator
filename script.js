1c88502703a27eebcd0b2e692255043d0f822938
Archangel <nathapcal@gmail.com>
2020-09-03 00:33:06 -0400

    was able to generate elements within the desired length (though they are undefined).  Need to further test with a TA.

--- script.js
+++ script.js
@@ -9,18 +9,38 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function generatePassword(onclick = "generateBtn") {
-  var storedPassword = ""
+function generatePassword() {
+  var password = "";
   var newPassword = "";
 
-  var checkPasswordLength = prompt("How many characters would you like the password to be?")
-    if ((isNaN(checkPasswordLength)) || (checkPasswordLength < 8) || checkPasswordLength > 128) {
-      alert("Please choose a number between 8 and 128.")
-      (generatePassword());
-    } else {
-      alert("null");
+  var checkPasswordLength = prompt(
+    "How many characters would you like the password to be?"
+  );
+
+  if (
+    isNaN(checkPasswordLength) ||
+    checkPasswordLength < 8 ||
+    checkPasswordLength > 128
+  ) {
+    alert("Please choose a number between 8 and 128.")(generatePassword());
+  } else {
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordUpperCase = confirm(
+      "Would you like the password to have upper case letters?"
+    );
+
+    if (passwordUpperCase == true) {
+      password = password + upperCase;
     }
   }
+
+  for (var i = 0; i < checkPasswordLength; i++) {
+    var random = Math.floor(Math.random() + password.length);
+    newPassword = newPassword + password[random];
+  }
+
+  return newPassword;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
