4d601a055676d969abe53344e3892a5ea03bf559
Archangel <nathapcal@gmail.com>
2020-09-02 21:31:43 -0400

    added function and return a random number

--- script.js
+++ script.js
@@ -10,34 +10,36 @@ function writePassword() {
 }
 
 function generatePassword(onclick = "generateBtn") {
-  //
+  
 
   // Prompts question for how long the password would be and stores value
-  var passwordLength = prompt("How long you you like the password to be");
-  // Sets Boolean
-  var lowerCase = true;
-  // lets the user to coose to use upper case and stores boolean
-  var passwordLowerCase = confirm("Would you like to include lower cases?");
-  // Sets boolean
-  var upperCase = true;
-  // lets the user to choose to use lower case and stores boolean
-  var passwordUpperCase = confirm("Would you like to include upper cases?");
-  // Sets boolean
-  var numbers = true;
+  // var passwordLength = prompt(
+  //   "How many characters would you you like the password to be?"
+  // );
+  // // Sets Boolean
+  // var lowerCase = true;
+  // // lets the user to coose to use upper case and stores boolean
+  // var passwordLowerCase = confirm("Would you like to include lower cases?");
+  // // Sets boolean
+  // var upperCase = true;
+  // // lets the user to choose to use lower case and stores boolean
+  // var passwordUpperCase = confirm("Would you like to include upper cases?");
+  // // Sets boolean
+  // var numbers = true;
   // lets the user to choose to use numbers and stores boolean.
-  var passwordNumbers = confirm("Would you like to include numbers?");
-  // Sets boolean
-  var characters = true;
-  // lets the user to choose to use special characters and stores booleans;
-  var passwordCharacters = confirm(
-    "Would you like to include special characters?"
-  );
-}
 
-//generating random numbers
-for (var i = 0; i <= passwordNumbers.length; i++) {
-  var passwordNumbersAnswer = Math.floor(Math.random() * 128 - 1);
-  console.log(passwordNumbersAnswer[i]);
+  var passwordNumbers = confirm("Would you like to include numbers?");
+  if (passwordNumbers === true) {
+    for (var i = 0; i < 10; i++) {
+      var passwordNumbers = Math.floor(Math.random() * 10) - 1;
+    }
+  }
+  // Sets boolean
+  // var characters = true;
+  // // lets the user to choose to use special characters and stores booleans;
+  // var passwordCharacters = confirm(
+  //   "Would you like to include special characters?"
+  return ;
 }
 
 // Add event listener to generate button
